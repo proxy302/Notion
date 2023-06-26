4d6286bf76d5a350bc9dbb70d59d37b3f8e11d5a
Jackie Li <li.linfei@foxmail.com>
2023-06-26 14:42:39 +0800

    add: drawer antd component in mobile env for header

--- themes/hexo-adswave/components/TopNavAds.js
+++ themes/hexo-adswave/components/TopNavAds.js
@@ -1,11 +1,23 @@
+import { Drawer, Space } from 'antd'
+import { MenuUnfoldOutlined } from '@ant-design/icons'
+import { useState } from 'react'
+
 /**
  *
  */
 const TopNavAds = props => {
+  const [open, setOpen] = useState(false)
+  const showDrawer = () => {
+    setOpen(true)
+  }
+  const onClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <>
+    <div className="header-ads">
       {/* desktop mode */}
-      <div className="header-ads hidden py-3 w-full bg-white shadow-lg fixed z-40 top-0 left-0 sm:grid grid-cols-3 gap-5 justify-items-center items-center">
+      <div className="header-ads-desktop hidden py-3 w-full bg-white shadow-lg fixed z-40 top-0 left-0 sm:grid grid-cols-3 gap-5 justify-items-center items-center">
         <div className="header-nav flex">
           <a className="mx-2 hover:underline" href="#">
             主页
@@ -39,7 +51,26 @@ const TopNavAds = props => {
           </a>
         </div>
       </div>
-    </>
+
+      {/* mobile mode */}
+      <div className="header-ads-mobile py-3 w-full bg-white shadow-lg fixed z-40 top-0 left-0 block sm:hidden">
+        <Space>
+          <MenuUnfoldOutlined className="text-xl px-6" onClick={showDrawer} />
+        </Space>
+        <Drawer
+          title="Basic Drawer"
+          placement="left"
+          closable={true}
+          onClose={onClose}
+          open={open}
+          key="left"
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </div>
+    </div>
   )
 }
 
