e508f40c5ee639423a8531e1ba35e3977593a26a
JerryMoo <wuchujie123@gmail.com>
2023-06-26 15:59:22 +0800

    Revert "finish: style for top-nav-ads"
    This reverts commit c75bef6226385d201a74c4883e60861aa3e0ccf8.

--- themes/hexo-adswave/components/TopNavAds.js
+++ themes/hexo-adswave/components/TopNavAds.js
@@ -3,20 +3,12 @@
  */
 const TopNavAds = props => {
   return (
-    <div className="header-ads py-3 w-full bg-white shadow-lg fixed z-40 top-0 left-0 grid grid-cols-3 gap-5 justify-items-center items-center">
-      <div className="header-nav flex">
-        <a className="mx-2 hover:underline" href="#">
-          主页
-        </a>
-        <a className="mx-2 hover:underline" href="#">
-          特点
-        </a>
-        <a className="mx-2 hover:underline" href="#">
-          价格
-        </a>
-        <a className="mx-2 hover:underline" href="#">
-          联系我们
-        </a>
+    <div className="header-ads h-12 w-full bg-gray-400 fixed z-40 top-0 left-0">
+      <div className="header-nav">
+        <a href="#">主页</a>
+        <a href="#">特点</a>
+        <a href="#">价格</a>
+        <a href="#">联系我们</a>
       </div>
       <div className="header-logo">
         <img
@@ -25,16 +17,9 @@ const TopNavAds = props => {
           alt=""
         />
       </div>
-      <div className="header-right flex items-center">
-        <a
-          href="#"
-          className="mx-2 px-4 py-1 rounded-full bg-[#5e79ee] text-white font-bold"
-        >
-          登录/注册
-        </a>
-        <a href="#" className="mx-2">
-          English
-        </a>
+      <div className="header-right">
+        <div>登录/注册</div>
+        <div>English</div>
       </div>
     </div>
   )
