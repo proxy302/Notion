db2b31dba9889f731970b6c44f33869504a8b259
Jackie Li <li.linfei@foxmail.com>
2023-06-26 11:28:29 +0800

    add: skull for top-nav-ads

--- themes/hexo-adswave/components/TopNavAds.js
+++ themes/hexo-adswave/components/TopNavAds.js
@@ -3,7 +3,25 @@
  */
 const TopNavAds = props => {
   return (
-    <div className=" h-12 w-full bg-gray-400 fixed z-40 top-0 left-0">aaa</div>
+    <div className="header-ads h-12 w-full bg-gray-400 fixed z-40 top-0 left-0">
+      <div className="header-nav">
+        <a href="#">主页</a>
+        <a href="#">特点</a>
+        <a href="#">价格</a>
+        <a href="#">联系我们</a>
+      </div>
+      <div className="header-logo">
+        <img
+          className="image w-36"
+          src="https://www.proxy302.com/assets/imgs/logo.png?_=1677746101"
+          alt=""
+        />
+      </div>
+      <div className="header-right">
+        <div>登录/注册</div>
+        <div>English</div>
+      </div>
+    </div>
   )
 }
 
