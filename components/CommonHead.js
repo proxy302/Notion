2fa6dfd9871a6e1ef65205f86760ba1e05b62d77
Jackie Li <li.linfei@foxmail.com>
2023-07-05 11:48:16 +0800

    update: title of html

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -8,7 +8,7 @@ const CommonHead = ({ meta, children }) => {
     url = `${url}/${meta.slug}`
     image = meta.image || '/bg_image.jpg'
   }
-  const title = meta?.title || BLOG.TITLE
+  const title = 'Proxy302文章资讯'
   const description = meta?.description || BLOG.DESCRIPTION
   const type = meta?.type || 'website'
   const keywords = meta?.tags || BLOG.KEYWORDS
@@ -19,7 +19,10 @@ const CommonHead = ({ meta, children }) => {
     <Head>
       <title>{title}</title>
       <meta name="theme-color" content={BLOG.BACKGROUND_DARK} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0"/>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0"
+      />
       <meta name="robots" content="follow, index" />
       <meta charSet="UTF-8" />
       {BLOG.SEO_GOOGLE_SITE_VERIFICATION && (
@@ -43,15 +46,24 @@ const CommonHead = ({ meta, children }) => {
 
       {BLOG.COMMENT_WEBMENTION.ENABLE && (
         <>
-          <link rel="webmention" href={`https://webmention.io/${BLOG.COMMENT_WEBMENTION.HOSTNAME}/webmention`} />
-          <link rel="pingback" href={`https://webmention.io/${BLOG.COMMENT_WEBMENTION.HOSTNAME}/xmlrpc`} />
+          <link
+            rel="webmention"
+            href={`https://webmention.io/${BLOG.COMMENT_WEBMENTION.HOSTNAME}/webmention`}
+          />
+          <link
+            rel="pingback"
+            href={`https://webmention.io/${BLOG.COMMENT_WEBMENTION.HOSTNAME}/xmlrpc`}
+          />
         </>
       )}
-      {BLOG.COMMENT_WEBMENTION.ENABLE && BLOG.COMMENT_WEBMENTION.AUTH !== '' && (
-        <link href={BLOG.COMMENT_WEBMENTION.AUTH} rel="me" />
-      )}
+      {BLOG.COMMENT_WEBMENTION.ENABLE &&
+        BLOG.COMMENT_WEBMENTION.AUTH !== '' && (
+          <link href={BLOG.COMMENT_WEBMENTION.AUTH} rel="me" />
+        )}
 
-      {JSON.parse(BLOG.ANALYTICS_BUSUANZI_ENABLE) && <meta name="referrer" content="no-referrer-when-downgrade" />}
+      {JSON.parse(BLOG.ANALYTICS_BUSUANZI_ENABLE) && (
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+      )}
       {meta?.type === 'Post' && (
         <>
           <meta
