809aaba3b4ca81b1f220aac879f65d0682009a10
Jerry Moo <jerrymoo@JerrydeMac-mini.local>
2023-10-31 18:31:41 +0800

    update

--- themes/hexo-adswave/components/AnnouncementBarAds.js
+++ themes/hexo-adswave/components/AnnouncementBarAds.js
@@ -6,7 +6,8 @@ const AnnouncementBarAds = () => {
     <div className="announcement-bar bg-[#0a1380] flex justify-center">
       <img
         className="object-cover h-10 sm:h-16"
-        src="/banner-proxy302-chinese.png"
+        // src="/banner-proxy302-chinese.png"
+        src="/banner-proxy302-3rd-cbinese.jpg"
         alt="discount"
       />
     </div>
