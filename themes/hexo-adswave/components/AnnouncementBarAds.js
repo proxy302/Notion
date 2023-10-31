f38a5515c2021f0a2110ff83f2e8287ec3a057c4
zhuangweiwu <zhuangweiwu05@qq.com>
2023-10-31 19:09:03 +0800

    fix: adjust banner

--- themes/hexo-adswave/components/AnnouncementBarAds.js
+++ themes/hexo-adswave/components/AnnouncementBarAds.js
@@ -3,7 +3,7 @@
  */
 const AnnouncementBarAds = () => {
   return (
-    <div className="announcement-bar bg-[#0a1380] flex justify-center">
+    <div className="announcement-bar bg-[#0d1c9b] flex justify-center">
       <img
         className="object-cover h-10 sm:h-16"
         // src="/banner-proxy302-chinese.png"
